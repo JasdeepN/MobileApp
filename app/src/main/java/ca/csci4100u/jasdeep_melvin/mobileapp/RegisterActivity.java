b0032f5e9272885a432a1a8b92a15986cb1a76f7
melvincheng <melvin.cheng@live.com>
2016-11-28 00:59:22 -0500

    Adding files

--- app/src/main/java/ca/csci4100u/jasdeep_melvin/mobileapp/RegisterActivity.java
+++ app/src/main/java/ca/csci4100u/jasdeep_melvin/mobileapp/RegisterActivity.java
@@ -3,12 +3,14 @@ package ca.csci4100u.jasdeep_melvin.mobileapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     AccountDBHelper helper;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +23,17 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View v) {
+
         TextView username = (TextView) findViewById(R.id.register_username_edit);
         TextView password = (TextView) findViewById(R.id.register_password_edit);
         TextView passwordConfirm = (TextView) findViewById(R.id.register_passwordConfirm_edit);
+
+        String usernameText = username.getText().toString();
+        String passwordText = password.getText().toString();
+        String confirmText = password.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(usernameText, passwordText);
+        auth.createUserWithEmailAndPassword()
 
         if(password.getText().toString().trim().length() <= 0 || username.getText().toString().trim().length() <= 0){
             Toast.makeText(getBaseContext(), R.string.register_blank_toast, Toast.LENGTH_SHORT).show();
